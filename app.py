f441d17eecd32e33d88af4ca1d242b4edbc4d7cb
Osawa-Tbrave <158064145+Osawa-Tbrave@users.noreply.github.com>
2024-05-07 00:19:41 +0900

    app.py

--- app.py
+++ app.py
@@ -32,21 +32,24 @@ scissors = '''
 
 choices = [rock, paper, scissors]
 
-your_choice = st.radio("What do you choose?", options=["Rock", "Paper", "Scissors"])
-your_choice_index = ["Rock", "Paper", "Scissors"].index(your_choice)
-cpu_choice_index = random.randint(0,2) # int
-
-# ユーザーの選択を表示
-st.write("Your choice:")
-st.code(choices[your_choice_index])
-
-# CPUの選択を表示
-st.write("CPU choice:")
-st.code(choices[cpu_choice_index])
-
-if your_choice_index == cpu_choice_index:
-  st.write("Draw")
-elif (your_choice_index == 0 and cpu_choice_index == 1) or (your_choice_index == 1 and cpu_choice_index == 2) or (your_choice_index == 2 and cpu_choice_index == 0):
-  st.write("You lose")
+your_choice = st.radio("What do you choose?", options=["選択してください", "Rock", "Paper", "Scissors"])
+if your_choice == "選択してください":
+    st.write("Please make a choice.")
 else:
-  st.write("You win")
+    your_choice_index = ["Rock", "Paper", "Scissors"].index(your_choice)
+    cpu_choice_index = random.randint(0,2) # int
+
+    # ユーザーの選択を表示
+    st.write("Your choice:")
+    st.code(choices[your_choice_index])
+
+    # CPUの選択を表示
+    st.write("CPU choice:")
+    st.code(choices[cpu_choice_index])
+
+    if your_choice_index == cpu_choice_index:
+      st.write("Draw")
+    elif (your_choice_index == 0 and cpu_choice_index == 1) or (your_choice_index == 1 and cpu_choice_index == 2) or (your_choice_index == 2 and cpu_choice_index == 0):
+      st.write("You lose")
+    else:
+      st.write("You win")
